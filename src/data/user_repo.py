ac74f75e4fd8a79813e2d8651d92b11c361b573b
Max Schneller <max@gmail.com>
2026-01-24 12:44:38 +0100

    Added and debugged get_user_id function

--- src/data/user_repo.py
+++ src/data/user_repo.py
@@ -7,3 +7,14 @@ def create_user(username: str) -> None:
         cur = conn.cursor()
         cur.execute("INSERT OR IGNORE INTO users (username) VALUES (?)", (username,))
         conn.commit()
+
+#Returns user id if such exists
+def get_user_id(username: str) -> int | None:
+    with get_connection() as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT id FROM users WHERE username = ?", (username,))
+        row = cur.fetchone()
+        if row == None:
+            return None
+        else:
+            return row["id"]
