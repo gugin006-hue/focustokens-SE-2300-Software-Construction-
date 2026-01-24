7eac71d1afabaa384559f84503e729c1c60dc65f
Max Schneller <max@gmail.com>
2026-01-24 13:10:02 +0100

    Added ensure_default_settings and get_settings functions and ran a test with inserting an id into the settings table

--- src/data/settings_repo.py
+++ src/data/settings_repo.py
@@ -2,3 +2,19 @@ from sqlite3 import Row
 from src.data.db import get_connection
 
 # Assigns default values to settings
+def ensure_default_settings() -> None:
+    daily_tokens = 10
+    normal_ping_cost = 1
+    urgent_ping_cost = 3
+    with get_connection() as conn:
+        cur = conn.cursor()
+        cur.execute("INSERT OR IGNORE INTO settings (id, daily_tokens, normal_ping_cost, urgent_ping_cost) VALUES (?, ?, ?, ?)", (1, daily_tokens, normal_ping_cost, urgent_ping_cost))
+        conn.commit()
+
+# Returns the settings tuple
+def get_settings() -> Row:
+    with get_connection() as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM settings WHERE id = 1")
+        row = cur.fetchone()
+        return row
